6c8ab96c8862719439e712cc3f43489b548cc003
Dima <dt@temesoft.com>
2026-03-26 10:57:38 -0700

    * Updated LocalStackContainer to remove account requirement

--- src/test/java/com/temesoft/fs/S3FileStorageServiceImplTest.java
+++ src/test/java/com/temesoft/fs/S3FileStorageServiceImplTest.java
@@ -17,7 +17,6 @@ import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.time.Duration;
 import java.util.Arrays;
 import java.util.UUID;
 
@@ -38,7 +37,7 @@ class S3FileStorageServiceImplTest {
     @Container
     private static final LocalStackContainer localstack = new LocalStackContainer(
             DockerImageName.parse("localstack/localstack:latest"))
-            .withStartupTimeout(Duration.ofMinutes(2))
+            .withEnv("LOCALSTACK_ACKNOWLEDGE_ACCOUNT_REQUIREMENT", "1")
             .withServices(S3);
 
     private static S3Client s3Client;
